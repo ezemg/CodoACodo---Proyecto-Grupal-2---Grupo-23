4d556e0bec448e3744d5e8e3b1bdddae512f4dd2
eze g <ezequielmatiasgr@gmail.com>
2023-11-28 19:56:08 -0300

    nueva prueba deploy

--- app/app.py
+++ app/app.py
@@ -1,7 +1,8 @@
 # app/app.py
 from flask import Flask, g
 from flask_cors import CORS
-from routes.cursos_routes import cursos_routes
+# RECIEN LO MODIFIQUE PARA PROBAR
+from app.routes.cursos_routes import cursos_routes
 from config import get_config
 from db import close_db, get_db
 from middlewares.middlewares import not_found
